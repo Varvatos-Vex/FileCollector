ae3a6b720cbfc55b250c6fb82a19c24b580655a7
Varvatos-Vex <aws.ki.testing@gmail.com>
2021-05-21 17:26:57 +0530

    TPI

--- FileCollector/settings.py
+++ FileCollector/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'collect'
+    'collect',
+    'user_visit',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "user_visit.middleware.UserVisitMiddleware",
 ]
 
 ROOT_URLCONF = 'FileCollector.urls'
